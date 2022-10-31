1508259eac42dd13acfeac5ce16fc9f51ca1a8cc
Dam <shit2wice@gmail.com>
2022-10-31 16:32:02 +0100

    chore: Add app components and dependencies

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import "./App.css";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Extensions from "./components/Extensions";
+import Questions from "./components/Questions";
+import CTA from "./components/CTA";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-      {/* <Header /> */}
-      {/* <Hero /> */}
-      {/* <Features /> */}
-      {/* <Extensions */}
-      {/* <Questions /> */}
-      {/* <CTA /> */}
-      {/* <Footer /> */}
+      <Header />
+      <Hero />
+      <Features />
+      <Extensions />
+      <Questions />
+      <CTA />
+      <Footer />
     </div>
   );
 }
