7973629d1e5c9b06be98d2fa013e3259d85a6bb6
Dam <shit2wice@gmail.com>
2022-11-01 21:21:38 +0100

    style: Add features header

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -40,16 +40,49 @@ const Features = () => {
 
 const FeaturesContainer = styled.div``;
 
-const FeaturesHeader = styled.div``;
-const FHeaderTitle = styled.div``;
-const FHeaderText = styled.div``;
+const FeaturesHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: 3rem 2rem;
+`;
+const FHeaderTitle = styled.div`
+  color: var(--vdark-blue);
+  font-size: 1.75rem;
+  font-weight: 500;
+  margin: 1rem;
+`;
+const FHeaderText = styled.div`
+  color: var(--grayish-blue);
+  line-height: 1.5;
+  text-align: center;
+  max-width: 550px;
+`;
 
 const FeaturesMain = styled.div``;
-const FMainHeader = styled.div``;
-const FMHeaderItem = styled.div``;
+const FMainHeader = styled.div`
+  border-bottom: 1px solid var(--grayish-blue);
+  margin: 0rem 2rem 3rem;
+`;
+const FMHeaderItem = styled.div`
+  border-top: 1px solid var(--grayish-blue);
+  padding: 1rem 0;
+  text-align: center;
+  color: var(--vdark-blue);
+  font-weight: 500;
+
+  :active {
+    border-bottom: 2px solid var(--soft-red);
+  }
+`;
+
 const FMainBody = styled.div``;
 const FMBodyLeft = styled.div``;
-const FeaturesImage = styled.img``;
+const FeaturesImage = styled.img`
+  width: 80%;
+`;
+
 const FMBodyRight = styled.div``;
 const FRightText = styled.div``;
 const FRightTitle = styled.div``;
