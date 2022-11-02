85d237189b1ecf8a1901dfa2c88b15877dc29d07
Dam <shit2wice@gmail.com>
2022-11-02 23:40:13 +0100

    style: Add features desktop view

--- src/components/Extensions.jsx
+++ src/components/Extensions.jsx
@@ -16,35 +16,42 @@ const Extensions = () => {
         </EHeaderText>
       </ExtensionsHeader>
 
-      <Extension>
-        <ExtensionLogo src={googleLogo} alt="google" />
-        <ExtensionTitle>Add to Chrome</ExtensionTitle>
-        <ExtensionText>Minimum version 62</ExtensionText>
-        <ExtensionDots src={dots} alt="dots" />
-        <ExtensionButton>Add & Install Extension</ExtensionButton>
-      </Extension>
-      <Extension>
-        <ExtensionLogo src={firefoxLogo} alt="firefox" />
-        <ExtensionTitle>Add to Firefox</ExtensionTitle>
-        <ExtensionText>Minimum version 55</ExtensionText>
-        <ExtensionDots src={dots} alt="dots" />
-        <ExtensionButton>Add & Install Extension</ExtensionButton>
-      </Extension>
-      <Extension>
-        <ExtensionLogo src={operaLogo} alt="opera" />
-        <ExtensionTitle>Add to Opera</ExtensionTitle>
-        <ExtensionText> Minimum version 46</ExtensionText>
-        <ExtensionDots src={dots} alt="dots" />
-        <ExtensionButton>Add & Install Extension</ExtensionButton>
-      </Extension>
+      <ExtensionBody>
+        <Extension>
+          <ExtensionLogo src={googleLogo} alt="google" />
+          <ExtensionTitle>Add to Chrome</ExtensionTitle>
+          <ExtensionText>Minimum version 62</ExtensionText>
+          <ExtensionDots src={dots} alt="dots" />
+          <ExtensionButton>Add & Install Extension</ExtensionButton>
+        </Extension>
+        <Extension>
+          <ExtensionLogo src={firefoxLogo} alt="firefox" />
+          <ExtensionTitle>Add to Firefox</ExtensionTitle>
+          <ExtensionText>Minimum version 55</ExtensionText>
+          <ExtensionDots src={dots} alt="dots" />
+          <ExtensionButton>Add & Install Extension</ExtensionButton>
+        </Extension>
+        <Extension>
+          <ExtensionLogo src={operaLogo} alt="opera" />
+          <ExtensionTitle>Add to Opera</ExtensionTitle>
+          <ExtensionText> Minimum version 46</ExtensionText>
+          <ExtensionDots src={dots} alt="dots" />
+          <ExtensionButton>Add & Install Extension</ExtensionButton>
+        </Extension>
+      </ExtensionBody>
     </ExtensionsContainer>
   );
 };
 
 const ExtensionsContainer = styled.div`
   padding: 2rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 const ExtensionsHeader = styled.div`
+  max-width: 550px;
   margin: 2rem 0;
   text-align: center;
 `;
@@ -59,7 +66,24 @@ const EHeaderText = styled.div`
   line-height: 1.5;
 `;
 
-const Extension = styled.div`
+const ExtensionBody = styled.div`
+  @media (min-width: 850px) {
+    display: flex;
+    justify-content: space-between;
+  }
+  section:nth-child(2) {
+    margin-top: 4rem;
+  }
+  section:nth-child(3) {
+    margin-top: 6rem;
+  }
+`;
+
+const Extension = styled.section`
+  flex: 1;
+  width: 100%;
+  max-width: 300px;
+  height: 360px;
   margin: 2rem 0;
   padding: 2rem;
   border-radius: 1rem;
@@ -67,6 +91,10 @@ const Extension = styled.div`
     -2px 0px 3px 0px rgba(0, 0, 0, 0.1);
 
   text-align: center;
+
+  @media (min-width: 850px) {
+    margin-right: 2rem;
+  }
 `;
 const ExtensionLogo = styled.img`
   margin: 1rem 0;
@@ -81,6 +109,7 @@ const ExtensionText = styled.div`
 `;
 const ExtensionDots = styled.img`
   margin: 1rem 0;
+  width: 100%;
 `;
 const ExtensionButton = styled.button`
   width: 100%;
