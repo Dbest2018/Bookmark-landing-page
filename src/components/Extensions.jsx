fed825c95af92744df753e69c04777ad10eb51b4
Dam <shit2wice@gmail.com>
2022-11-02 23:15:16 +0100

    style: Add extensions mobile view

--- src/components/Extensions.jsx
+++ src/components/Extensions.jsx
@@ -1,7 +1,96 @@
 import React from "react";
+import styled from "styled-components";
+import googleLogo from "../images/logo-chrome.svg";
+import firefoxLogo from "../images/logo-firefox.svg";
+import operaLogo from "../images/logo-opera.svg";
+import dots from "../images/bg-dots.svg";
 
 const Extensions = () => {
-  return <div>Extensions</div>;
+  return (
+    <ExtensionsContainer>
+      <ExtensionsHeader>
+        <EHeaderTitle>Download the extension</EHeaderTitle>
+        <EHeaderText>
+          We’ve got more browsers in the pipeline. Please do let us know if
+          you’ve got a favourite you’d like us to prioritize.
+        </EHeaderText>
+      </ExtensionsHeader>
+
+      <Extension>
+        <ExtensionLogo src={googleLogo} alt="google" />
+        <ExtensionTitle>Add to Chrome</ExtensionTitle>
+        <ExtensionText>Minimum version 62</ExtensionText>
+        <ExtensionDots src={dots} alt="dots" />
+        <ExtensionButton>Add & Install Extension</ExtensionButton>
+      </Extension>
+      <Extension>
+        <ExtensionLogo src={firefoxLogo} alt="firefox" />
+        <ExtensionTitle>Add to Firefox</ExtensionTitle>
+        <ExtensionText>Minimum version 55</ExtensionText>
+        <ExtensionDots src={dots} alt="dots" />
+        <ExtensionButton>Add & Install Extension</ExtensionButton>
+      </Extension>
+      <Extension>
+        <ExtensionLogo src={operaLogo} alt="opera" />
+        <ExtensionTitle>Add to Opera</ExtensionTitle>
+        <ExtensionText> Minimum version 46</ExtensionText>
+        <ExtensionDots src={dots} alt="dots" />
+        <ExtensionButton>Add & Install Extension</ExtensionButton>
+      </Extension>
+    </ExtensionsContainer>
+  );
 };
+
+const ExtensionsContainer = styled.div`
+  padding: 2rem;
+`;
+const ExtensionsHeader = styled.div`
+  margin: 2rem 0;
+  text-align: center;
+`;
+const EHeaderTitle = styled.div`
+  margin: 1rem 0;
+  color: var(--vdark-blue);
+  font-size: 1.75rem;
+  font-weight: 500;
+`;
+const EHeaderText = styled.div`
+  color: var(--grayish-blue);
+  line-height: 1.5;
+`;
+
+const Extension = styled.div`
+  margin: 2rem 0;
+  padding: 2rem;
+  border-radius: 1rem;
+  box-shadow: 2px 10px 10px 0px rgba(0, 0, 0, 0.1),
+    -2px 0px 3px 0px rgba(0, 0, 0, 0.1);
+
+  text-align: center;
+`;
+const ExtensionLogo = styled.img`
+  margin: 1rem 0;
+`;
+const ExtensionTitle = styled.div`
+  color: var(--vdark-blue);
+  font-weight: 500;
+`;
+const ExtensionText = styled.div`
+  color: var(--grayish-blue);
+  margin: 1rem 0;
+`;
+const ExtensionDots = styled.img`
+  margin: 1rem 0;
+`;
+const ExtensionButton = styled.button`
+  width: 100%;
+  padding: 1rem;
+  font-family: "Rubik", san-serif;
+  color: white;
+  cursor: pointer;
+  background-color: var(--soft-blue);
+  border: none;
+  border-radius: 0.3rem;
+`;
 
 export default Extensions;
