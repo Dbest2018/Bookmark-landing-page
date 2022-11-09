2875411ea4621af6254652d1ea5fa6919ac90466
Dam <shit2wice@gmail.com>
2022-11-09 17:31:26 +0100

    feat: Add navigation functionality on mobile view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,46 @@
-import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import headerLogo from "../images/logo-bookmark.svg";
 import headerMenu from "../images/icon-hamburger.svg";
+import closeIcon from "../images/icon-close.svg";
+import navFacebook from "../images/icon-facebook.svg";
+import navTwitter from "../images/icon-twitter.svg";
 
 const Header = () => {
+  const [openNav, setOpenNav] = useState(true);
   return (
     <HeaderContainer>
+      {openNav && (
+        <MobileNav>
+          <MNavHeader>
+            <NHeaderLogo src={headerLogo} alt="logo" />
+            <NHeaderIcon
+              src={closeIcon}
+              alt="close"
+              onClick={() => setOpenNav(false)}
+            />
+          </MNavHeader>
+          <MNavBody>
+            <NBodyItem>FEATURES</NBodyItem>
+            <NBodyItem>PRICING</NBodyItem>
+            <NBodyItem>CONTACT</NBodyItem>
+            <NBodyButton>LOGIN</NBodyButton>
+          </MNavBody>
+          <MNavFooter>
+            <NFooterIcon src={navFacebook} alt="facebook" />
+            <NFooterIcon src={navTwitter} alt="twitter" />
+          </MNavFooter>
+        </MobileNav>
+      )}
       <HeaderLeft>
         <LeftLogo src={headerLogo} alt="logo" />
       </HeaderLeft>
       <HeaderRight>
-        <RightMenu src={headerMenu} alt="hamburger-menu" />
+        <RightMenu
+          src={headerMenu}
+          alt="hamburger-menu"
+          onClick={() => setOpenNav(true)}
+        />
         <RightNav>
           <NavItem>FEATURES</NavItem>
           <NavItem>PRICING</NavItem>
@@ -70,5 +100,60 @@ const HeaderButton = styled.button`
     transform: scale(1.2);
   }
 `;
+const MobileNav = styled.div`
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: var(--vdark-blue);
+  z-index: 1000;
+  opacity: 0.9;
+  height: 100vh;
+  padding: 2rem;
+`;
+const MNavHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-bottom: 2rem;
+  border-bottom: 1px solid var(--grayish-blue);
+`;
+const NHeaderLogo = styled.img`
+  color: white;
+`;
+const NHeaderIcon = styled.img``;
+const MNavBody = styled.div`
+  color: white;
+  text-align: center;
+`;
+const NBodyItem = styled.div`
+  border-bottom: 1px solid var(--grayish-blue);
+  padding: 1rem 0;
+`;
+const NBodyButton = styled.button`
+  background-color: inherit;
+  width: 100%;
+  border: 2px solid white;
+  padding: 0.5rem 0;
+  color: white;
+  cursor: pointer;
+  letter-spacing: 0.1rem;
+  font-family: "Rubik", san-serif;
+  font-size: 1.25rem;
+  font-weight: 500;
+  margin: 1rem 0;
+  border-radius: 0.25rem;
+`;
+const MNavFooter = styled.div`
+  position: fixed;
+  bottom: 2rem;
+  right: 2rem;
+  left: 2rem;
+  display: flex;
+  justify-content: center;
+  gap: 3rem;
+`;
+const NFooterIcon = styled.img``;
 
 export default Header;
